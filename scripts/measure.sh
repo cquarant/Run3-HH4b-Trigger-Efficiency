00de71695f6931ea07d33474b70e51edcea3fb72
Zichun Hao <zichunh2000@gmail.com>
2025-02-27 12:39:15 -0600

    Script for the entire workflow

---
+++ scripts/measure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd );
+cd ${SCRIPT_DIR}; cmsenv;
+
+# SF(mass, pt)
+cd ${SCRIPT_DIR}/sf_mass_pt; 
+./histo_data.csh; ./histo_mc.csh;
+./trig_eff_mass_pt.csh; 
+
+# SF(TXbb)
+cd ${SCRIPT_DIR}/sf_TXbb;
+./histo_data.csh; ./histo_mc.csh;
+./trig_eff_TXbb.csh;
+
+# TTbar background correction
+cd ${SCRIPT_DIR}/semi-leptonic;
+./tree_data.csh; ./tree_mc.csh;
+./derive_SF.csh;
